efc6bf1e5356e0ff94d5de62e26201ee9ed412f6
inwisloklem <89099109104@mail.ru>
2017-04-09 18:32:56 +0300

    Add fireball colors array

--- js/setup.js
+++ js/setup.js
@@ -39,6 +39,14 @@ var EYES_COLORS = [
   'green'
 ];
 
+var FIREBALL_COLORS = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848'
+];
+
 var wizardDetails = {
   'firstNames': FIRST_NAMES,
   'lastNames': LAST_NAMES,
